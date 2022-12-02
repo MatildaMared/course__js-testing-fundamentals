cab7277285c29eaf43c3eef28a4484df03cbfc6f
Matilda Mared <matildamared@live.se>
2022-12-02 10:44:55 +0100

    initialize project

---
+++ lessons/simple.js
@@ -0,0 +1,22 @@
+/**
+ * We have two functions, `sum` and `subtract` ,
+ * that are ready to be tested.
+ *
+ * Task: check if the results are the respects.
+ *
+ *            **BONUS**
+ * Throw an error if the test fails.
+ *
+ *
+ * Execute: Use `node lessons/simple.js` to run the test.
+ */
+
+const {sum, subtract} = require('../math')
+
+let result, expected
+
+/**
+ * Hint: there's no magic, just the most straightforward conditional statement.
+ *
+ * Answer: Checkout the main branch for the answer.
+ */
